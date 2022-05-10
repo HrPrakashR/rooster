0f56bb950dd25fb2aef0a04b90f6391a6faab02a
Philipp <mail@philipp-bomers.de>
2022-05-10 19:09:09 +0200

    cleanup

--- src/main/java/com/example/rooster/RoosterApplication.java
+++ src/main/java/com/example/rooster/RoosterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoosterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoosterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoosterApplication.class, args);
+    }
 
 }
