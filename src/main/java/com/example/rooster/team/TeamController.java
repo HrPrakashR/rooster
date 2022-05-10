0e784644b83bb06f8e68c3260adfa22faa06352e
Philipp <mail@philipp-bomers.de>
2022-05-10 21:09:41 +0200

    added constructors for teamservice and teamcontroller

--- src/main/java/com/example/rooster/team/TeamController.java
+++ src/main/java/com/example/rooster/team/TeamController.java
@@ -5,7 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TeamController {
 
+    private TeamService teamService;
+
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+
     // Team anlegen
+
 
     // Team bearbeiten
 
--- src/main/java/com/example/rooster/team/TeamService.java
+++ src/main/java/com/example/rooster/team/TeamService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TeamService {
+    private final TeamRepository teamRepository;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
 }
