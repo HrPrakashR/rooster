4142342b3c09bb84b5a05d67cd374c816b07fd7a
mkatran <mehmetkatran@gmail.com>
2022-05-11 11:35:18 +0200

    Added Mappings for PeriodRequest, added necessary methods in service.

--- src/main/java/com/example/rooster/period/PeriodController.java
+++ src/main/java/com/example/rooster/period/PeriodController.java
@@ -1,15 +1,15 @@
 package com.example.rooster.period;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import com.example.rooster.employee.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class PeriodController {
 
     // neuen Eintrag erstellen
@@ -32,6 +32,24 @@ public class PeriodController {
                                 period.getDateTo(),
                                 period.getEmployee()))
                 .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    @GetMapping("/periodRequest")
+    public PeriodDTO createPeriodRequest() {
+        long miliseconds = System.currentTimeMillis();
+        Date today = new Date(miliseconds);
+        Date tomorrow = new Date(miliseconds + 86_400_000); //miliseconds in a day
+        //Today and tomorrow are the default days for the request
+        PeriodDTO periodDTO = new PeriodDTO(Purpose.WORKING_HOURS, today, tomorrow, new Employee());
+        return periodDTO;
+    }
+
+    //TODO: We need the model attribute Session user to be able to save the request
+    @PostMapping("/periodRequest")
+    public List<Period> submitPeriodRequest(PeriodDTO periodDTO) {
+        Period period = periodService.convertToPeriod(periodDTO);
+        periodService.addPeriod(period);
+        return periodService.findPeriodsByEmployee(period.getEmployee());
     }
 
     // Eintrag anzeigen
