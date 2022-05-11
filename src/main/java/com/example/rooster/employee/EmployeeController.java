ff78a70cd69201ffcc8f80dc60ef6cc7a17409d3
Philipp <mail@philipp-bomers.de>
2022-05-11 09:40:09 +0200

    added CrossOrigin to work with Spring Boot Rest API

--- src/main/java/com/example/rooster/employee/EmployeeController.java
+++ src/main/java/com/example/rooster/employee/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.example.rooster.employee;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
--- src/main/java/com/example/rooster/team/TeamController.java
+++ src/main/java/com/example/rooster/team/TeamController.java
@@ -1,8 +1,10 @@
 package com.example.rooster.team;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TeamController {
 
     private final TeamService teamService;
