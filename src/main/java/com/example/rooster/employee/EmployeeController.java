3f1baa33f268d3cd2eea09108e46394f1ec3ea9e
Philipp <mail@philipp-bomers.de>
2022-05-11 12:14:35 +0200

    removed crossirigin

--- src/main/java/com/example/rooster/employee/EmployeeController.java
+++ src/main/java/com/example/rooster/employee/EmployeeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
--- src/main/java/com/example/rooster/team/TeamController.java
+++ src/main/java/com/example/rooster/team/TeamController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class TeamController {
 
     private final TeamService teamService;
