e1941f7d96dc2d0042b0097dd6f4269965fe9d38
Philipp <mail@philipp-bomers.de>
2022-05-11 14:14:25 +0200

    Rest API now works with Angular and Spring Boot

--- rooster-web/src/app/employee/employee.component.ts
+++ rooster-web/src/app/employee/employee.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Observable, Subscription, throwError} from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import {Component, Injectable, OnInit} from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
+@Injectable()
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  employees: Employee[] | undefined;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<Employee[]>('/api/employees').subscribe(employees => {
+      this.employees = employees;
+    });
   }
-
 }
