d3dcb07e65fe03452a8bea558fe714af2345adc3
Philipp <mail@philipp-bomers.de>
2022-05-11 12:05:27 +0200

    added all entities from the backend

--- rooster-web/src/app/app.module.ts
+++ rooster-web/src/app/app.module.ts
@@ -4,23 +4,27 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import {RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
-
-const appRoutes: Routes = [
-  { path: 'employee', component: EmployeeComponent },
-];
+import { PeriodComponent } from './period/period.component';
+import { TeamComponent } from './team/team.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    PeriodComponent,
+    TeamComponent,
+    GeneratorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    RouterModule.forRoot([
+      { path: 'employee', component: EmployeeComponent },
+      { path: 'period', component: PeriodComponent },
+      { path: 'team', component: TeamComponent },
+      { path: 'generator', component: GeneratorComponent },
+    ])
   ],
   bootstrap: [AppComponent]
 })
