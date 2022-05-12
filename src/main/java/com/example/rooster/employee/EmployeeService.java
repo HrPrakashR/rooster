4e8198f5922cae3241f7a2d0608bcee6dfd875f8
Êlias <elias.akbari@yahoo.de>
2022-05-12 14:48:05 +0200

    added and edited methodes in EmployeeService, EmployeeRepository and EmployeeController:
    EmployeeController: use DTOs instead of Entities
    EmployeeService: new converter methodes + deleteEmployeeByEmail & findEmployeeByEmail
    EmployeeRepository: findEmployeeByEmail

--- src/main/java/com/example/rooster/employee/EmployeeService.java
+++ src/main/java/com/example/rooster/employee/EmployeeService.java
@@ -3,6 +3,7 @@ package com.example.rooster.employee;
 import com.example.rooster.team.Team;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,8 +18,46 @@ public class EmployeeService {
         return this.employeeRepository.findEmployeeById(id);
     }
 
+    public Employee convertToEmployee(EmployeeDTO employeeDTO){
+        Employee employee = new Employee();
+        employee.setFirstName(employeeDTO.getFirstName());
+        employee.setLastName(employeeDTO.getLastName());
+        employee.setEmail(employeeDTO.getEmail());
+        employee.setPassword(employeeDTO.getPassword());
+        employee.setTeam(employeeDTO.getTeam());
+        employee.setHoursPerWeek(employeeDTO.getHoursPerWeek());
+        employee.setBalanceHours(employeeDTO.getBalanceHours());
+        employee.setBreakTime(employeeDTO.getBreakTime());
+        employee.setRole(employeeDTO.getRole());
+        return employee;
+    }
+
+    public EmployeeDTO convertToDTO(Employee employee){
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        employeeDTO.setFirstName(employee.getFirstName());
+        employeeDTO.setLastName(employee.getLastName());
+        employeeDTO.setEmail(employee.getEmail());
+        employeeDTO.setPassword(employee.getPassword());
+        employeeDTO.setTeam(employee.getTeam());
+        employeeDTO.setHoursPerWeek(employee.getHoursPerWeek());
+        employeeDTO.setBalanceHours(employee.getBalanceHours());
+        employeeDTO.setBreakTime(employee.getBreakTime());
+        employeeDTO.setRole(employee.getRole());
+        return employeeDTO;
+    }
+
+
+
     public List<Employee> getEmployees() {
+
         return this.employeeRepository.findAll();
+    }
+
+    public List<EmployeeDTO> getEmployeesAsDTO(){
+        List<Employee> employees = this.getEmployees();
+        List<EmployeeDTO> employeeDTOS = new ArrayList<>();
+        employees.forEach(employee -> employeeDTOS.add(this.convertToDTO(employee)));
+        return employeeDTOS;
     }
 
     public List<Employee> getEmployees(Team team) {
@@ -33,11 +72,13 @@ public class EmployeeService {
         return this.employeeRepository.save(employee);
     }
 
-    public void deleteEmployeeById(long id) {
-        this.employeeRepository.deleteById(id);
+    public Employee getEmployeeByEmail(String email) {
+
+        return this.employeeRepository.findEmployeeByEmail(email);
     }
 
-    public void updateFirstName(long id, String firstName) {
-        this.employeeRepository.updateFirstName(id, firstName);
+    public void deleteEmployeeByEmail(String email ) {
+        this.employeeRepository.delete(this.getEmployeeByEmail(email));
     }
+
 }
