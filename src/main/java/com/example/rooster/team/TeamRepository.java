aafa5e3fc48582ad7ffc3305ee4f00289e6a6f17
Prakash Rathinasamy <mr.prakash.rathinasamy@gmail.com>
2022-05-12 12:08:22 +0200

    changed CRUD Repository to JPA Repository in Team, all Mappings are done with TeamDTO

--- src/main/java/com/example/rooster/team/TeamRepository.java
+++ src/main/java/com/example/rooster/team/TeamRepository.java
@@ -1,12 +1,12 @@
 package com.example.rooster.team;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TeamRepository extends CrudRepository<Team, Long> {
+public interface TeamRepository extends JpaRepository<Team, Long> {
 
     List<Team> findAll();
 
