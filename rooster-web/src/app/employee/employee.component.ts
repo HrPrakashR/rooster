8df99b9ba2c11486efab350877671e8c36c7c9e9
mkatran <mehmetkatran@gmail.com>
2022-05-13 14:22:42 +0200

    Added new employee form and employee list to html. Added necessary methods to component.

--- rooster-web/src/app/employee/employee.component.ts
+++ rooster-web/src/app/employee/employee.component.ts
@@ -1,6 +1,8 @@
 import {HttpClient} from '@angular/common/http';
 import {Component, Injectable, OnInit} from '@angular/core';
 import {Employee} from './employee';
+import {Observable} from "rxjs";
+import {Team} from "../team/team";
 
 @Component({
   selector: 'app-employee',
@@ -11,17 +13,39 @@ import {Employee} from './employee';
 export class EmployeeComponent implements OnInit {
 
   employees?: Employee[];
+  teams?: Team[];
+  newEmployee = {} as Employee;
+  isFormShown = false;
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
-    this.getEmployee();
+    this.getEmployees();
+    this.getTeams();
   }
 
-  getEmployee() {
+  getEmployees() {
     this.http
       .get<Employee[]>('/api/employees/get_all')
       .subscribe(result => this.employees = result);
+  }
+
+  getTeams() {
+    this.http
+      .get<Team[]>('/api/teams/get_all')
+      .subscribe(result => this.teams = result);
+  }
+
+  saveNewEmployee(newEmployee: Employee): Observable<Employee> {
+    return this.http.post<Employee>("/api/employees/new", newEmployee);
+  }
+
+  toggleShow() {
+    this.isFormShown = ! this.isFormShown;
+  }
+
+  clearAll() {
+    this.employees = undefined;
   }
 }
