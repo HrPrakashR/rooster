a58f3828e3b7dead4dc54d637aa1acae21cb7fc2
Prakash Rathinasamy <mr.prakash.rathinasamy@gmail.com>
2022-05-13 08:02:14 +0200

    created Methods in period.component.ts and in period.component.html

--- rooster-web/src/app/period/period.component.ts
+++ rooster-web/src/app/period/period.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Period} from './period';
 import {HttpClient} from "@angular/common/http";
 
-
 @Component({
   selector: 'app-period',
   templateUrl: './period.component.html',
@@ -13,10 +12,31 @@ export class PeriodComponent implements OnInit {
   newPeriod: Period = {} as Period;
   periods?: Period[];
 
+  newLeave = false;
+
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
+  }
+
+  fetchAll() {
+    this.http.get<Period[]>('/api/periods/get_all').subscribe(result => this.periods = result);
+  }
+
+  clearAll() {
+    this.periods = undefined;
+  }
+
+  showLeaveRequest(){
+    this.newLeave = !this.newLeave;
+  }
+
+  saveEntry(){
+    this.http.post<Period[]>('/api/periods/new', this.newPeriod)
+      .subscribe( np => this.periods = np);
+    // this.periods?.push(this.newPeriod);
+    this.newPeriod = {} as Period;
   }
 
 }
