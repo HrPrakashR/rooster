a0a2dc2b70c71efaf5970a3ac388b21f0c2525c7
Philipp <mail@philipp-bomers.de>
2022-05-14 12:50:12 +0200

    added email functionality to send password

--- src/main/java/com/example/rooster/user/UserController.java
+++ src/main/java/com/example/rooster/user/UserController.java
@@ -1,6 +1,11 @@
 package com.example.rooster.user;
 
 import com.example.rooster.employee.EmployeeService;
+import com.example.rooster.team.Team;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,11 @@ public class UserController {
 
     public UserController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @PostMapping("/sendPassword/{userId}")
+    public ResponseEntity<String> sendPassword(@PathVariable long userId) {
+        String email = this.employeeService.getEmployeeById(userId).getEmail();
+        return new ResponseEntity<>("Successfully sent", HttpStatus.OK);
     }
 }
