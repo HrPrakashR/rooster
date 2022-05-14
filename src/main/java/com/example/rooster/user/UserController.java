1a92890a97268b4c7c37e2651b80ad904c09c9c9
Philipp <mail@philipp-bomers.de>
2022-05-14 13:29:50 +0200

    we are able to send emails

--- src/main/java/com/example/rooster/user/UserController.java
+++ src/main/java/com/example/rooster/user/UserController.java
@@ -1,27 +1,41 @@
 package com.example.rooster.user;
 
 import com.example.rooster.employee.EmployeeService;
-import com.example.rooster.team.Team;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users/")
 public class UserController {
 
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
+    private final JavaMailSender emailSender;
 
-    public UserController(EmployeeService employeeService) {
+    public UserController(EmployeeService employeeService, JavaMailSender emailSender) {
         this.employeeService = employeeService;
+        this.emailSender = emailSender;
     }
 
-    @PostMapping("/sendPassword/{userId}")
+    @GetMapping("/sendPassword/{userId}")
     public ResponseEntity<String> sendPassword(@PathVariable long userId) {
         String email = this.employeeService.getEmployeeById(userId).getEmail();
+        this.sendPassword(email);
         return new ResponseEntity<>("Successfully sent", HttpStatus.OK);
+    }
+
+    public void sendPassword(String to) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("javamailer545@gmail.com");
+        message.setTo(to);
+        message.setSubject("Your new Password");
+        message.setText("New Password: "+ this.getPassword());
+        emailSender.send(message);
+    }
+
+    private String getPassword() {
+        return "12345";
     }
 }
