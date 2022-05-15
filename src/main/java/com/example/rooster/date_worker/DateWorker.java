a7cdede47489e3b7c4a8f2e9e9be307980bad404
Philipp <mail@philipp-bomers.de>
2022-05-15 16:53:35 +0200

    added class for date generations

--- src/main/java/com/example/rooster/date_worker/DateWorker.java
+++ src/main/java/com/example/rooster/date_worker/DateWorker.java
@@ -4,17 +4,17 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateWorker {
-    public static Date getDate(boolean lastDay, int year, int month) {
+    public static Date getDate(boolean lastDayOfMonth, int year, int month) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.YEAR, year);
         calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, lastDay ? calendar.getActualMaximum(Calendar.DAY_OF_MONTH) : calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+        calendar.set(Calendar.DAY_OF_MONTH, lastDayOfMonth ? calendar.getActualMaximum(Calendar.DAY_OF_MONTH) : calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
 
-        calendar.set(Calendar.AM_PM, lastDay ? Calendar.PM : Calendar.AM);
-        calendar.set(Calendar.MILLISECOND, lastDay? calendar.getActualMaximum(Calendar.MILLISECOND) : calendar.getActualMinimum(Calendar.MILLISECOND));
-        calendar.set(Calendar.SECOND, lastDay? calendar.getActualMaximum(Calendar.SECOND) : calendar.getActualMinimum(Calendar.SECOND));
-        calendar.set(Calendar.MINUTE, lastDay? calendar.getActualMaximum(Calendar.MINUTE) : calendar.getActualMinimum(Calendar.MINUTE));
-        calendar.set(Calendar.HOUR, lastDay? calendar.getActualMaximum(Calendar.HOUR) : calendar.getActualMinimum(Calendar.HOUR));
+        calendar.set(Calendar.AM_PM, lastDayOfMonth ? Calendar.PM : Calendar.AM);
+        calendar.set(Calendar.MILLISECOND, lastDayOfMonth? calendar.getActualMaximum(Calendar.MILLISECOND) : calendar.getActualMinimum(Calendar.MILLISECOND));
+        calendar.set(Calendar.SECOND, lastDayOfMonth? calendar.getActualMaximum(Calendar.SECOND) : calendar.getActualMinimum(Calendar.SECOND));
+        calendar.set(Calendar.MINUTE, lastDayOfMonth? calendar.getActualMaximum(Calendar.MINUTE) : calendar.getActualMinimum(Calendar.MINUTE));
+        calendar.set(Calendar.HOUR, lastDayOfMonth? calendar.getActualMaximum(Calendar.HOUR) : calendar.getActualMinimum(Calendar.HOUR));
 
         return calendar.getTime();
     }
