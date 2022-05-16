14a32a5dd9765bb98cdddb621bf941780537d037
philipp <mail@philipp-bomers.de>
2022-05-16 16:32:58 +0200

    working on bug in correct display months

--- rooster-web/src/app/generator/generator.component.ts
+++ rooster-web/src/app/generator/generator.component.ts
@@ -7,28 +7,57 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GeneratorComponent implements OnInit {
 
+  // 0 = January
   month: number = 0;
   year: number = 2022;
-  days: [number] = [1];
 
+  days: [number] = [1];
   yearsToChoose: [number] = [2022];
   monthToChoose: [number] = [0];
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit(): void {
     this.month = new Date().getMonth();
     this.createCalendar();
   }
 
-  ngOnInit(): void {}
-
-  getMonth(value: number){
-    return value+1;
+  getMonthName(month: number){
+    switch (month){
+      case 0:
+        return "January";
+      case 1:
+        return "February";
+      case 3:
+        return "March";
+      case 4:
+        return "April";
+      case 5:
+        return "May";
+      case 6:
+        return "June";
+      case 7:
+        return "August";
+      case 8:
+        return "September";
+      case 9:
+        return "October";
+      case 10:
+        return "November";
+      case 11:
+        return "December";
+      default:
+        return "Unknown Month";
+    }
   }
 
   createCalendar(){
     this.days = [1];
+    this.yearsToChoose = [2022];
+    this.monthToChoose = [0];
+
     let i = 1;
-    while(new Date(this.year, this.month, i).getMonth() == this.month){
+    while(new Date(this.year, this.month, i).getMonth().valueOf() == this.month){
       if(i<=11){
         this.monthToChoose.push(i);
       }
