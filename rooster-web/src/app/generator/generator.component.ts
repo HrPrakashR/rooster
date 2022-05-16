38de8344f25e6dc69a02c18c8e6271dc1ed4828a
philipp <mail@philipp-bomers.de>
2022-05-16 16:04:02 +0200

    calendar generation with selecting month and year works

--- rooster-web/src/app/generator/generator.component.ts
+++ rooster-web/src/app/generator/generator.component.ts
@@ -7,35 +7,38 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GeneratorComponent implements OnInit {
 
-  month: number = 2;
+  month: number = 0;
   year: number = 2022;
   days: [number] = [1];
 
   yearsToChoose: [number] = [2022];
-  monthToChoose: [number] = [1];
+  monthToChoose: [number] = [0];
 
   constructor() {
-    this.month = new Date().getMonth()+1;
+    this.month = new Date().getMonth();
     this.createCalendar();
   }
 
   ngOnInit(): void {}
 
-  setMonth(readableMonth: number){
-    this.month = readableMonth-1;
+  getMonth(value: number){
+    return value+1;
   }
 
   createCalendar(){
     this.days = [1];
-    let i = 2;
+    let i = 1;
     while(new Date(this.year, this.month, i).getMonth() == this.month){
-      if(i<=12){
+      if(i<=11){
         this.monthToChoose.push(i);
       }
       if(i<20){
-        this.yearsToChoose.push(i);
+        this.yearsToChoose.push(this.yearsToChoose[0].valueOf() + i);
       }
-      this.days.push(i++);
+      if(i>1){
+        this.days.push(i);
+      }
+      i++;
     }
   }
 
