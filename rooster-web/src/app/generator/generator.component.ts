bc7bb1873e114bee0a9bf016a646ae2942464edd
philipp <mail@philipp-bomers.de>
2022-05-16 15:49:42 +0200

    show generator table (in progress)

--- rooster-web/src/app/generator/generator.component.ts
+++ rooster-web/src/app/generator/generator.component.ts
@@ -7,10 +7,36 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GeneratorComponent implements OnInit {
 
+  month: number = 2;
+  year: number = 2022;
+  days: [number] = [1];
+
+  yearsToChoose: [number] = [2022];
+  monthToChoose: [number] = [1];
+
   constructor() {
+    this.month = new Date().getMonth()+1;
+    this.createCalendar();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setMonth(readableMonth: number){
+    this.month = readableMonth-1;
+  }
+
+  createCalendar(){
+    this.days = [1];
+    let i = 2;
+    while(new Date(this.year, this.month, i).getMonth() == this.month){
+      if(i<=12){
+        this.monthToChoose.push(i);
+      }
+      if(i<20){
+        this.yearsToChoose.push(i);
+      }
+      this.days.push(i++);
+    }
   }
 
 }
