e4e9296acde53b727639aa9f4f927f09cf6c89a2
mkatran <mehmetkatran@gmail.com>
2022-05-16 15:33:22 +0200

    Changed the order of methods for better readability.
    Non-Void methods now return exclusively DTOs.

--- src/main/java/com/example/rooster/employee/EmployeeController.java
+++ src/main/java/com/example/rooster/employee/EmployeeController.java
@@ -16,37 +16,40 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-
-    @GetMapping("/get_all")
-    List<EmployeeDTO> getEmployees() {
-        return employeeService.getEmployeesAsDTO();
-    }
-
-    @PostMapping("/new")
-    public Employee addNewEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        return employeeService.setEmployee(employeeService.convertToEmployee(employeeDTO));
-    }
-
-    // ToDo: Use DTO instead of ID and search employee by email (like new team service method)
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable long id) {
-        employeeService.deletePeriodsOfEmployee(employeeService.getEmployeeById(id));
-        employeeService.deleteEmployeeById(id);
-        return new ResponseEntity<>("Successfully Deleted", HttpStatus.OK);
-    }
-
-
-    // ToDo: do the same with a DTO and post mapping, without path-variable
+    //Showing a certain employee
     @GetMapping("/get/{id}")
     public EmployeeDTO getEmployee(@PathVariable long id) {
         return this.employeeService.convertToDTO(employeeService.getEmployeeById(id));
     }
 
+    //Showing all employees
+    @GetMapping("/get_all")
+    List<EmployeeDTO> getEmployees() {
+        return employeeService.getEmployeesAsDTO();
+    }
 
-    // ToDo: do the same with a DTO and post mapping, without path-variable
+    //Creating a new employee
+    @PostMapping("/new")
+    public EmployeeDTO addNewEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        Employee newEmployee = employeeService.convertToEmployee(employeeDTO);
+        employeeService.setEmployee(newEmployee);
+        return employeeService.convertToDTO(newEmployee);
+    }
+
+    //Editing an existing employee
     @PostMapping("/edit")
-    public Employee updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        return employeeService.setEmployee(employeeService.convertToEmployee(employeeDTO));
+    public EmployeeDTO updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        Employee newEmployee = employeeService.convertToEmployee(employeeDTO);
+        employeeService.setEmployee(newEmployee);
+        return employeeService.convertToDTO(newEmployee);
+    }
+
+    //Deleting an existing employee
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable long id) {
+        employeeService.deletePeriodsOfEmployee(employeeService.getEmployeeById(id));
+        employeeService.deleteEmployeeById(id);
+        return new ResponseEntity<>("Successfully Deleted", HttpStatus.OK);
     }
 
 }
