2a581c40ab2e0e9c8aff28237b928591d1cf8848
Êlias <elias.akbari@yahoo.de>
2022-05-17 18:23:56 +0200

    added AuthService.java in auth package

---
+++ src/main/java/com/example/rooster/security/auth/AuthService.java
@@ -0,0 +1,37 @@
+package com.example.rooster.security.auth;
+
+import com.example.rooster.employee.Employee;
+import com.example.rooster.employee.EmployeeService;
+import com.example.rooster.security.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+
+@Service
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final EmployeeService employeeService;
+//    private final UserService userService;
+
+    public AuthService(PasswordEncoder passwordEncoder, EmployeeService employeeService) {
+        this.passwordEncoder = passwordEncoder;
+        this.employeeService = employeeService;
+    }
+
+    protected Employee login(String email, String password) {
+        Employee employee = this.employeeService.findEmployeeByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+
+        if (passwordEncoder.matches(password, employee.getPassword())) {
+            return employee;
+        } else {
+            throw new BadCredentialsException(format("Wrong password for user '%s'.", email));
+        }
+    }
+}
