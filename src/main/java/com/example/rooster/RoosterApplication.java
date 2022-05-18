6030369d2c00e89f96483a923ef3f9d4086c1dd7
philipp <mail@philipp-bomers.de>
2022-05-18 09:24:41 +0200

    removed unused import

--- src/main/java/com/example/rooster/RoosterApplication.java
+++ src/main/java/com/example/rooster/RoosterApplication.java
@@ -3,8 +3,6 @@ package com.example.rooster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
-
 @SpringBootApplication
 public class RoosterApplication {
 
