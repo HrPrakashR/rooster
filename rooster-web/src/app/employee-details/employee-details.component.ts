c3a02f8e3f7a6acef9926b6f0c2c026d83e200ec
mkatran <mehmetkatran@gmail.com>
2022-05-19 17:20:43 +0200

    added necessary variables and methods

--- rooster-web/src/app/employee-details/employee-details.component.ts
+++ rooster-web/src/app/employee-details/employee-details.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {Employee} from "../employee/employee";
 import {EmployeeService} from "../employee/employee.service";
+import {Role} from "../employee/role";
+import {Team} from "../team/team";
 
 @Component({
   selector: 'app-employee-details',
@@ -11,15 +13,49 @@ import {EmployeeService} from "../employee/employee.service";
 export class EmployeeDetailsComponent implements OnInit {
 
   employee?: Employee;
+  editMode = false;
+  Role = Role;
+  @Input() teams?: any[]
+  roleNames = Object.keys(Role);
+
   constructor(private employeeService: EmployeeService, private route:ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')
-    this.employeeService.getEmployee(Number(id)).subscribe(result => this.employee = result);;
+    this.employeeService.getEmployee(Number(id)).subscribe(result => this.employee = result);
+    this.getTeams();
   }
 
+//TODO: Write the duplicate methods in EmployeeService
   goBack() {
     this.router.navigate(['/employee']);
+  }
+
+  getTeams() {
+    this.employeeService.getTeams()
+      .subscribe(result => this.teams = result);
+  }
+
+  getTeamName(id: number) {
+    // @ts-ignore
+    return this.teams?.find(t => t.id === id).name;
+  }
+
+  editModeOn() {
+    this.editMode = true;
+  }
+
+  public editModeOff() {
+    this.editMode = false;
+  }
+
+  editEmployee(employee: Employee) {
+    this.employeeService.editEmployee(employee).subscribe();
+    this.editMode = false;
+  }
+
+  capitalizeFirstLetter(word: string) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
   }
 
 }
