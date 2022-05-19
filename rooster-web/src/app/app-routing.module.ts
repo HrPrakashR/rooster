e1e6ab595dfeb0bf49eb639a6582fbfb3addad29
mkatran <mehmetkatran@gmail.com>
2022-05-19 17:17:40 +0200

    Added Routes to routing module

--- rooster-web/src/app/app-routing.module.ts
+++ rooster-web/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {EmployeeComponent} from "./employee/employee.component";
+import {EmployeeDetailsComponent} from "./employee-details/employee-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employee', component: EmployeeComponent},
+  {path: 'employee/:id', component: EmployeeDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
