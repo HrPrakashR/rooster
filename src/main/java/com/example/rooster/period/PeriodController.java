61104117eaac202d36d63d0b127d93d2deecdb02
philipp <mail@philipp-bomers.de>
2022-05-21 13:21:25 +0200

    cleanup

--- src/main/java/com/example/rooster/period/PeriodController.java
+++ src/main/java/com/example/rooster/period/PeriodController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @RequestMapping("/api/periods/")
