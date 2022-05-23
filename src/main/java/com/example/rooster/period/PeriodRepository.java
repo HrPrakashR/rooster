e85cd6dd35b510db6ad13a9aca8b3957592cbc6b
mkatran <mehmetkatran@gmail.com>
2022-05-23 22:51:09 +0200

    created javadoc for Period Repository

--- src/main/java/com/example/rooster/period/PeriodRepository.java
+++ src/main/java/com/example/rooster/period/PeriodRepository.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Repository;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * JPA Repository for Period Entity
+ */
 @Repository
 public interface PeriodRepository extends JpaRepository<Period, Long> {
 
@@ -21,7 +24,21 @@ public interface PeriodRepository extends JpaRepository<Period, Long> {
 
     List<Period> findAllByEmployeeTeam(Team team);
 
+    /**
+     * Query to select periods of a team within a given time interval
+     * @param employee_team team of the given employee
+     * @param dateFrom first day of the time interval
+     * @param dateFrom2 last day of the time interval
+     * @return a list of Periods for the given parameters
+     */
     List<Period> findAllByEmployeeTeamAndDateFromBetween(Team employee_team, Date dateFrom, Date dateFrom2);
 
+    /**
+     * Query to select periods of an employee within a given time interval
+     * @param employee selected employee
+     * @param start first day of the time interval
+     * @param end last day of the time interval
+     * @return a list of Periods for the given parameters
+     */
     List<Period> findAllByEmployeeAndDateFromBetween(Employee employee, Date start, Date end);
 }
