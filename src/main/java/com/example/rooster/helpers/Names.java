d50337638ee96c59a0d85756c904de5d68da5a5a
mkatran <mehmetkatran@gmail.com>
2022-05-23 17:20:50 +0200

    added Muster Rooster

--- src/main/java/com/example/rooster/helpers/Names.java
+++ src/main/java/com/example/rooster/helpers/Names.java
@@ -5,7 +5,7 @@ import com.example.rooster.employee.Role;
 import java.util.List;
 
 public class Names {
-    public static final List<String> names = List.of("Robert", "Christopher", "Michael", "William", "John", "James", "Brian", "Thomas", "Matthew", "Scott", "Eric", "Mark", "Richard", "Lisa", "Amy", "Mary", "Jennifer", "Angela", "Michelle", "Melissa", "Susan", "Anne", "Patricia", "Julie", "Karen");
-    public static final List<String> lastNames = List.of("Bridges", "Koch", "Ward", "Goodman", "Alexander", "Peterson", "Klein", "Meadows", "Ramos", "Avila", "Delacruz", "Farmer", "Sherman", "Schwartz", "Holden", "Sweeney", "Mooney", "Freeman", "Bullock", "Olson", "Hanson", "Lawrence", "Riggs", "Randall", "Berg");
+    public static final List<String> names = List.of("Muster", "Christopher", "Michael", "William", "John", "James", "Brian", "Thomas", "Matthew", "Scott", "Eric", "Mark", "Richard", "Lisa", "Amy", "Mary", "Jennifer", "Angela", "Michelle", "Melissa", "Susan", "Anne", "Patricia", "Julie", "Karen");
+    public static final List<String> lastNames = List.of("Rooster", "Koch", "Ward", "Goodman", "Alexander", "Peterson", "Klein", "Meadows", "Ramos", "Avila", "Delacruz", "Farmer", "Sherman", "Schwartz", "Holden", "Sweeney", "Mooney", "Freeman", "Bullock", "Olson", "Hanson", "Lawrence", "Riggs", "Randall", "Berg");
 
 }
